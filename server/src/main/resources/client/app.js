27e11ab3d1fe247f6cc046bdffab91cbb736ef59
Patrick Osborne <patrick.osborne@petrasystems.com>
2017-12-21 22:45:34 -0700

    log version info

--- server/src/main/resources/client/app.js
+++ server/src/main/resources/client/app.js
@@ -1,42 +1,41 @@
 'use strict';
 
-var appModule = angular.module( 'JangularApp.app', ['ngRoute', 'JangularApp.shared', 'JangularApp.rest'] );
+var appModule = angular.module('JangularApp.app', ['ngRoute', 'JangularApp.shared', 'JangularApp.rest']);
 
-appModule.config( ['$routeProvider', function( $routeProvider ) {
-    $routeProvider.when( '/', {templateUrl:'home/home.html', controller:'Home'} );
-    $routeProvider.when( '/about', {templateUrl:'about/about.html', controller:'About'} );
-    $routeProvider.otherwise( {redirectTo:'/'} )
-}] );
+appModule.config(['$routeProvider', function ($routeProvider) {
+    $routeProvider.when('/', {templateUrl: 'home/home.html', controller: 'Home'});
+    $routeProvider.when('/about', {templateUrl: 'about/about.html', controller: 'About'});
+    $routeProvider.otherwise({redirectTo: '/'})
+}]);
 
-appModule.controller( 'appController', function( $scope, $location, $window, restCommon ) {
-    console.log( 'appController init' );
+appModule.controller('appController', function ($scope, $location, $window, restCommon) {
+    console.log('appController init');
 
-    function navigateTo( place ) {
-        console.log( 'navigateTo: place: ' + place );
-        if ( place )
-        {
-            $location.path( '/' + place );
+    function navigateTo(place) {
+        console.log('navigateTo: place: ' + place);
+        if (place) {
+            $location.path('/' + place);
         }
-        else
-        {
-            $location.path( '/' );
+        else {
+            $location.path('/');
         }
     }
 
     function redirectHome() {
         var url = '/client/index.html';
-        console.log( url );
+        console.log(url);
         $window.location.href = url;
     }
 
-    function handleVersionSuccess( versionData ) {
+    function handleVersionSuccess(versionData) {
+        console.log(versionData);
         $scope.version = 'Version: ' + versionData.version;
         $scope.versionData = versionData;
     }
 
-    function handleVersionError( status, data ) {
-        console.log( status );
-        console.log( data );
+    function handleVersionError(status, data) {
+        console.log(status);
+        console.log(data);
         $scope.version = 'version lookup error';
     }
 
@@ -44,10 +43,10 @@ appModule.controller( 'appController', function( $scope, $location, $window, res
         $scope.version = '';
         $scope.navigateTo = navigateTo;
 
-        restCommon.version( handleVersionSuccess, handleVersionError );
+        restCommon.version(handleVersionSuccess, handleVersionError);
     }
 
     init();
 
-} );
+});
 
