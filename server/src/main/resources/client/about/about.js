fecae23c6064674b4f333a2b4ef8f227dc110bb3
Patrick Osborne <patrick.osborne@petrasystems.com>
2017-12-21 23:01:18 -0700

    fixed header navigation. wired in version info into about tab.

--- server/src/main/resources/client/about/about.js
+++ server/src/main/resources/client/about/about.js
@@ -1,3 +1,24 @@
-appModule.controller( 'About', function( $scope, $location ) {
+appModule.controller('About', function ($scope, $location, restCommon) {
     console.log( 'About init' );
+
+    function handleVersionSuccess(versionData) {
+        console.log(versionData);
+        $scope.version = 'Version: ' + versionData.data.version;
+        $scope.versionData = versionData.data;
+    }
+
+    function handleVersionError(status, data) {
+        console.log(status);
+        console.log(data);
+        $scope.version = 'version lookup error';
+    }
+
+    function init() {
+        $scope.version = '';
+
+        restCommon.version(handleVersionSuccess, handleVersionError);
+    }
+
+    init();
+
 } );
